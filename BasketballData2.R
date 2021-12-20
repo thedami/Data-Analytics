eb7fa1973b9f49adae1131138e51c444939e4cf1
thedami <90115764+thedami@users.noreply.github.com>
2021-12-20 12:37:48 +0100

    Add files via upload

--- BasketballData2.R
+++ BasketballData2.R
@@ -1,35 +1,3 @@
-#Dear Student,
-#
-#Welcome to the dataset for the homework exercise.
-#
-#Instructions for this dataset:
-# You have only been supplied vectors. You will need
-# to create the matrices yourself.
-# Matrices:
-# - FreeThrows
-# - FreeThrowAttempts
-#
-#Sincerely,
-#Kirill Eremenko
-#www.superdatascience.com
-
-#Copyright: These datasets were prepared using publicly available data.
-#           However, theses scripts are subject to Copyright Laws. 
-#           If you wish to use these R scripts outside of the R Programming Course
-#           by Kirill Eremenko, you may do so by referencing www.superdatascience.com in your work.
-
-#Comments:
-#Seasons are labeled based on the first year in the season
-#E.g. the 2012-2013 season is preseneted as simply 2012
-
-#Notes and Corrections to the data:
-#Kevin Durant: 2006 - College Data Used
-#Kevin Durant: 2005 - Proxied With 2006 Data
-#Derrick Rose: 2012 - Did Not Play
-#Derrick Rose: 2007 - College Data Used
-#Derrick Rose: 2006 - Proxied With 2007 Data
-#Derrick Rose: 2005 - Proxied With 2007 Data
-
 #Seasons
 Seasons <- c("2005","2006","2007","2008","2009","2010","2011","2012","2013","2014")
 
